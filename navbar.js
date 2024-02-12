1b65ed0eb003fe0ed48999a9aba2c061712e3876
Sydney Alexis <133697035+sydthewebdev@users.noreply.github.com>
2024-02-12 16:43:07 -0600

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,5 @@
+// Navbar Functionality
+function toggleNavbar(collapseID) {
+    document.getElementById(collapseID).classList.toggle("hidden");
+    document.getElementById(collapseID).classList.toggle("block");
+  }
